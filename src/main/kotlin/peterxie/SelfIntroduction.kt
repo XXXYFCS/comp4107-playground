6a65ca708329cc7fef3b7a047d561b3fb98f4d7a
Your Name <your-email@example.com>
2025-03-04 20:54:09 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/peterxie/SelfIntroduction.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.peterxie
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Peter Xie.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
